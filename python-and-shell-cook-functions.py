36951c30f70635bca6b7e8fef155ea8b118032ee
haiyangd <donghy2007@hotmail.com>
2017-03-29 11:56:30 +0800

    Update python-and-shell-cook-functions.py

--- python-and-shell-cook-functions.py
+++ python-and-shell-cook-functions.py
@@ -16,3 +16,46 @@ def makePassword(minlength=5,maxlength=25):
   length=random.randint(minlength,maxlength) 
   letters=string.ascii_letters+string.digits # alphanumeric, upper and lowercase 
   return ''.join([random.choice(letters) for _ in range(length)]) 
+
+
+
+Shell 获取指定网卡IP，兼容公司各种Linux
+
+现在现网Tlinux2.2已经越来越多了，之前shell使用 ifconfig 来获取指定网卡IP已不再通用，因此抽空写了个脚本，基于 ip 命令获取指定网卡的IP地址，理论上支持ip命令的Linux都是兼容的。
+
+Ps：另附上网上找的Python版本，经供参考。
+
+#!/bin/bash
+###############################################
+# 获取指定网卡的IP地址, 兼容现网所有Linux版本     #
+# 已测试的系统：Tlinux 、SUSE、Slackware、Centos#
+# Usage：                                     # 
+# eth1ip=$(get_ip_of_interface)               #
+# eth0ip=$(get_ip_of_interface eth0)          #
+###############################################
+
+get_ip_of_interface()
+{
+   local iface=${1:-eth1}
+   /sbin/ip addr | grep "$iface" 2>/dev/null | \
+   awk -F '[/ ]+' '/inet / {print $3}'
+   # 返回grep的状态，可用于判断$iface是否存在
+   return ${PIPESTATUS[1]}
+}
+
+
+################### 华丽的分割线 ###################
+#!/usr/bin/python
+# Python 版本获取网卡IP
+# 可 shell 调用： eth1ip=$(python get_ip.py) 
+import socket
+import fcntl
+import struct
+def get_ip_address(ifname):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    return socket.inet_ntoa(fcntl.ioctl(
+        s.fileno(),
+        0x8915,  # SIOCGIFADDR
+        struct.pack('256s', ifname[:15])
+    )[20:24])
+print get_ip_address('eth1') #这样就获取了eth1的IP地址了
