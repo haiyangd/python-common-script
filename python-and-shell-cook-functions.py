93827d0f8232d749e79417218288af3c5ad442cf
haiyangd <donghy2007@hotmail.com>
2017-03-30 10:22:56 +0800

    Update python-and-shell-cook-functions.py

--- python-and-shell-cook-functions.py
+++ python-and-shell-cook-functions.py
@@ -59,3 +59,36 @@ def get_ip_address(ifname):
         struct.pack('256s', ifname[:15])
     )[20:24])
 print get_ip_address('eth1') #这样就获取了eth1的IP地址了
+
+shell脚本重试函数 
+支持设置重试次数，命令成功时的退出码，重试间隔时间
+# Usage: retry retry_max_times success_exit_code sleep_seconds_between_failure command_and_argument
+function retry {
+  if [ $# -le 3 ]; then
+	  echo "Usage: retry retry_max_times success_exit_code sleep_seconds_between_failure command_and_argument"
+	  return 2
+  fi
+  local retry_max=$1
+  local succ_exit_code=$2
+  local sleep_seconds=$3
+  shift 3
+ 
+  local count=$retry_max
+  while [ $count -gt 0 ]; do
+    "$@"
+	if [ $? -eq ${succ_exit_code} ]; then
+		break;
+	fi
+    count=$(($count - 1))
+    sleep $sleep_seconds
+  done
+ 
+  [ $count -eq 0 ] && {
+    echo "Retry failed [$retry_max]: $@" >&2
+    return 1
+  }
+  return 0
+}
+
+# 重试三次，命令成功时的退出码为0，失败时sleep 10s
+retry 3 0 10 ping foo.bar
