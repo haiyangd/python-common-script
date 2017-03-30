6edea9ba1fb4a852601da37fcce6d77acded4886
haiyangd <donghy2007@hotmail.com>
2017-03-30 10:36:30 +0800

    Update python-and-shell-cook-functions.py

--- python-and-shell-cook-functions.py
+++ python-and-shell-cook-functions.py
@@ -106,3 +106,106 @@ for ((i = 0; i < 20; i++))
 do
         echo "./a.out $i"
 done | xargs -0 -I'{}' -n1 -P4 sh -c '{}'
+
+通过shell脚本添加删除crontab任务 
+很多时候，比如在实现后台程序的自动化安装卸载的时候，我们需要通过脚本自动化添加删除crontab任务。假设我们需要针对程序安装目录下的start.sh
+添加删除crontab任务，我们在当前目录下创建addcrontab.sh和delcrontab.sh脚本：
+############## version 0.2 -- 考虑重复问题(感谢riverzheng的指点) ###########
+
+### addcrontab.sh ###
+#! /bin/sh
+
+unset IFS
+unset OFS
+unset LD_PRELOAD
+unset LD_LIBRARY_PATH
+export PATH='/usr/sbin:/sbin:/usr/bin:/bin'
+
+# 指定为哪个用户添加任务项
+user="root"
+
+check_user()
+{
+	curr="`whoami`"
+    if [[ "root" != $curr || $user != $curr ]]; then
+        echo "permission denied"
+        exit 1
+    fi    
+}
+
+check_exist()
+{
+   str="$(crontab -u $user -l | grep "$1")"
+   if [ -n "$str" ]; then
+       echo "crontab item existed"
+       exit 1
+   fi  
+}
+
+check_user
+# 获取当前脚本的目录
+dir="$( cd "$( dirname $0 )" && pwd )"
+bin="$dir/start.sh"
+check_exist $bin
+# 创建添加到crontab的任务项，可以根据实际情况构造，比如接受传入参数进行构造
+line="*/1 * * * * $bin > /dev/null 2>&1 &"
+# 将任务项添加到crontab列表
+(crontab -u $user -l; echo "$line") | crontab -u $user -
+
+
+### delcrontab.sh ###
+#! /bin/sh
+
+check_user
+dir="$( cd "$( dirname $0 )" && pwd )"
+line="$dir/start.sh"
+# 从crontab列表删除指定任务项
+(crontab -u $user -l | grep -v "$line") | crontab -u $user -
+
+
+### delcrontab.sh ###
+#! /bin/sh
+
+check_user
+dir="$( cd "$( dirname $0 )" && pwd )"
+line="$dir/start.sh"
+# 从crontab列表删除指定任务项
+(crontab -u $user -l | grep -v "$line") | crontab -u $user -
+
+########################## version 0.1 ##########################
+
+### addcrontab.sh ###
+#! /bin/sh
+
+# 指定为哪个用户添加任务项
+user="root"
+
+check_user()
+{
+	curr="`whoami`"
+    if [[ "root" != $curr || $user != $curr ]]; then
+        echo "permission denied"
+        exit 1
+    fi    
+}
+
+check_user
+# 获取当前脚本的目录
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# 创建添加到crontab的任务项，可以根据实际情况构造，比如接受传入参数进行构造
+line="*/1 * * * * $dir/start.sh > /dev/null 2>&1 &"
+# 将任务项添加到crontab列表
+(crontab -u $user -l; echo "$line") | crontab -u $user -
+
+
+
+
+### delcrontab.sh ###
+#! /bin/sh
+
+check_user
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+line="$dir/start.sh"
+# 从crontab列表删除指定任务项
+(crontab -u $user -l | grep -v "$line") | crontab -u $user -      
+      
