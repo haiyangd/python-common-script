9c87daba01217a62b9b192cb3bc4b387d4b1d530
haiyangd <donghy2007@hotmail.com>
2017-03-31 11:29:22 +0800

    Update python-and-shell-cook-functions.py

--- python-and-shell-cook-functions.py
+++ python-and-shell-cook-functions.py
@@ -361,3 +361,13 @@ cat a b | sort | uniq
 
 # a 交 b
 cat a b | sort | uniq -d
+    
+Getting MAC Address
+#!/usr/bin/python
+import fcntl, socket, struct
+def getHwAddr(ifname):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    info = fcntl.ioctl(s.fileno(), 0x8927,  struct.pack('256s', ifname[:15]))
+    return ':'.join(['%02x' % ord(char) for char in info[18:24]])
+
+print getHwAddr('eth0')
