9ae0a9440ebaffc486eb042934048b89351417b4
haiyangd <donghy2007@hotmail.com>
2017-04-06 14:51:22 +0800

    Update python-and-shell-cook-functions.py

--- python-and-shell-cook-functions.py
+++ python-and-shell-cook-functions.py
@@ -747,3 +747,43 @@ if __name__ == '__main__':
     foo(a=1,b=2,c=3)
     foo(1,2,3,4, a=1,b=2,c=3)
 foo('a', 1, None, a=1, b='2', c=3)
+
+###########################################
+ dnsmap.py
+ #!/bin/python
+
+"""
+Based on WebMap: https://github.com/4rsh/python/blob/master/webmap.py
+DNSMap - Developed by Arsh Leak.
+$ wget https://github.com/4rsh/
+"""
+
+# Colours
+D  = "\033[0m";  
+W  = "\033[01;37m";  
+O  = "\033[01;33m"; 
+SUCESS = "\033[01;32m";
+FAIL = "\033[01;31m";
+
+import socket
+import sys
+import os
+os.system("clear")
+print O+ "+----------------------------------------------------------------------------+"
+print "|                                      DNSMap                                |"
+print "+----------------------------------------------------------------------------+"
+print "|                            Development by Arsh Leak.                       |"
+print "|                         $ Wget > http://github.com/4rsh                    |"
+print "+----------------------------------------------------------------------------+"
+print W+""
+domain = raw_input("Set domain: ") # www.domain.com
+ 
+try:
+    ip = socket.gethostbyname( domain )
+ 
+except socket.gaierror:
+    print FAIL+'Invalid Domain.\n\n\n\n\n\n\n'
+    sys.exit()
+print SUCESS+"+-------------------------+"
+print SUCESS+"| DNS   : " +ip+ "     |"
+print SUCESS+"+-------------------------+"
