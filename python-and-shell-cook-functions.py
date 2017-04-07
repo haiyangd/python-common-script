9aa9d4fc79257718f4971b185a6ce34c5258d4b8
haiyangd <donghy2007@hotmail.com>
2017-04-07 22:40:50 +0800

    Update python-and-shell-cook-functions.py

--- python-and-shell-cook-functions.py
+++ python-and-shell-cook-functions.py
@@ -1718,3 +1718,30 @@ def _str_to_num(n):
     return val
 print _str_to_num(n='0x0a')
 print _str_to_num(n='\\21')
+###############################
+[root@VM_132_108_centos python]# cat tmp1.py 
+import os
+import glob
+
+"""
+One day, when this module grows up, it might actually try to fix things.
+'apt-cache search | apt-get install' ... or a less terrifying version of
+the same. With added distro-independant pixie dust.
+"""
+
+def command(cmd):
+    # this could use '/usr/bin/which', I suppose. But this seems simpler
+    for dir in os.environ['PATH'].split(':'):
+        file = os.path.join(dir, cmd)
+        if os.path.exists(file):
+            return file
+    raise ValueError('Missing command: %s' % cmd)
+
+print command('ls')
+def commands(cmds):
+    results = []
+    for cmd in cmds:
+        print cmd
+        results.append(command(cmd))
+    return results
+print commands(['ls','pwd'])
